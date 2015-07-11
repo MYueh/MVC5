b808ac39cefeaf02e465bf3ebb0ffcba34813b4f
Will <doggy.huang@gmail.com>
2015-07-11 11:52:49 +0800

    13 用 Entity Framework 實作「取得 ProductId > 1500 的產品」列表 ( /EF/Index )

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -11,7 +11,24 @@ namespace MVC5Course.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            #region Option 1
+
+            using (var db = new FabricsEntities())
+            {
+                var data = db.Product.Where(p => p.ProductId > 1500).ToList();
+
+                return View(data);
+            }
+
+            #endregion
+
+            #region Option 2
+
+            //var db = new FabricsEntities();
+            //var data = db.Product.Where(p => p.ProductId > 1500);
+            //return View(data);
+
+            #endregion
         }
 
         public int AddProduct()
