a08020f3c278d95390c3b9edabea272cd33d85f9
Will <doggy.huang@gmail.com>
2015-07-11 12:08:46 +0800

    重構資料庫連線，將 FabricsEntities 移到類別層級，並實作 Dispose 方法中斷資料庫連線

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -9,48 +9,55 @@ namespace MVC5Course.Controllers
 {
     public class EFController : Controller
     {
+        private FabricsEntities db = new FabricsEntities();
+
         public ActionResult Index()
         {
             #region Option 1
 
-            using (var db = new FabricsEntities())
-            {
-                var data = db.Product.Where(p => p.ProductId > 1500).ToList();
+            //using (var db = new FabricsEntities())
+            //{
+            //    var data = db.Product.Where(p => p.ProductId > 1500).ToList();
 
-                return View(data);
-            }
+            //    return View(data);
+            //}
 
             #endregion
 
             #region Option 2
 
-            //var db = new FabricsEntities();
-            //var data = db.Product.Where(p => p.ProductId > 1500);
-            //return View(data);
+            var data = db.Product.Where(p => p.ProductId > 1500);
+
+            return View(data);
 
             #endregion
         }
 
         public int AddProduct()
         {
-            using (var db = new FabricsEntities())
+            var product = new Product()
             {
-                var product = new Product()
-                {
-                    ProductName = "T-Shirt",
-                    Price = 199,
-                    Active = true,
-                    Stock = 10
-                };
+                ProductName = "T-Shirt",
+                Price = 199,
+                Active = true,
+                Stock = 10
+            };
 
-                db.Product.Add(product);
+            db.Product.Add(product);
 
-                db.SaveChanges();
+            db.SaveChanges();
 
-                return product.ProductId;
-            }
+            return product.ProductId;
         }
 
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
     }
 }
