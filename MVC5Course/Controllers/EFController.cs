71dc8ed6f1a7a407243f7276110c0f3fa6eaaa1c
Will <doggy.huang@gmail.com>
2015-07-11 11:10:05 +0800

    12 用 Entity Framework 實作「新增產品」功能 ( /EF/AddProduct )

---
+++ MVC5Course/Controllers/EFController.cs
@@ -0,0 +1,39 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class EFController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public int AddProduct()
+        {
+            using (var db = new FabricsEntities())
+            {
+                var product = new Product()
+                {
+                    ProductName = "T-Shirt",
+                    Price = 199,
+                    Active = true,
+                    Stock = 10
+                };
+
+                db.Product.Add(product);
+
+                db.SaveChanges();
+
+                return product.ProductId;
+            }
+        }
+
+
+    }
+}
