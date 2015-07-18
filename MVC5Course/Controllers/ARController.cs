2bfb9678954e3a0c39e27618d6675f7499c8c754
Will <doggy.huang@gmail.com>
2015-07-18 14:01:05 +0800

    23 練習 ContentResult 與 FileResult 的各種用法

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -10,12 +10,35 @@ namespace MVC5Course.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            string str = "Hello, World";
+
+            return View((object)str);
         }
 
         public ActionResult PartialViewTest()
         {
             return PartialView("MyPartial");
+        }
+
+        public ActionResult ContentTest()
+        {
+            return Content("<h1>ContentTest</h1>", "text/plain");
+        }
+        public ActionResult ContentTest2()
+        {
+            //Response.Write("<script>alert('新增成功'); location.href='/';</script>");
+            return Content("<script>alert('新增成功'); location.href='/';</script>");
+        }
+        public ActionResult ContentTest3()
+        {
+            ViewBag.msg = "新增成功";
+
+            return View("AlertRedirect");
+        }
+
+        public ActionResult FileTest()
+        {
+            return File(Server.MapPath("~/Content/hqdefault.jpg"), "image/jpeg", "泛舟哥.jpg");
         }
     }
 }
