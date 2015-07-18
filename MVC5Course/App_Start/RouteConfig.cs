9dd5b724bcd2065f74f18fdd6252c136e6d0912d
Will <doggy.huang@gmail.com>
2015-07-18 10:03:35 +0800

    19 練習自訂路由參數

--- MVC5Course/App_Start/RouteConfig.cs
+++ MVC5Course/App_Start/RouteConfig.cs
@@ -11,12 +11,47 @@ namespace MVC5Course
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            /*
+             * http://localhost/Home/Cart.aspx?id=123
+             */
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            /*
+             * http://api.jquery.com/jQuery.each/
+             */
+            routes.MapRoute(
+                name: "Practics1",
+                url: "{controller}.{action}.{id}",
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
+            );
+
+            /*
+             * http://api.jquery.com/Docs/jQuery-each/
+             */
+            routes.MapRoute(
+                name: "Practics2",
+                url: "Docs/{controller}-{action}/{id}",
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
+            );
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new {
+                    controller = "Home", 
+                    action = "Index", 
+                    id = UrlParameter.Optional 
+                }
             );
         }
     }
