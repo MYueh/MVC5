9959b6100a35910835abe78d1469168628da6e4b
Will <doggy.huang@gmail.com>
2015-07-18 13:23:48 +0800

    22 練習 ViewResult 與 PartialViewResult 的各種用法

---
+++ MVC5Course/Controllers/ARController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ARController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PartialViewTest()
+        {
+            return PartialView("MyPartial");
+        }
+    }
+}
--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace MVC5Course.Controllers
             return View();
         }
 
+        public ActionResult PartialViewTest()
+        {
+            return PartialView("MyPartial");
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
