7388bedda3a2e5979c1228ac2cefec78192cf26a
Will <doggy.huang@gmail.com>
2015-07-18 15:21:03 +0800

    24 練習建立 ViewModel、套用模型繫結、表單驗證、使用 TempData 傳資料

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,35 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(SampleVM data)
+        {
+            // 寫入資料
+            if (ModelState.IsValid)
+            {
+                TempData["IndexSaveMsg"] = "新增" + data.Name + "成功";
+                return RedirectToAction("IndexResult");
+            }
+
+            return View(data);
+        }
+
+        public ActionResult IndexResult()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC5Course/Models/SampleVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public class SampleVM
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Title { get; set; }
+    }
+}
