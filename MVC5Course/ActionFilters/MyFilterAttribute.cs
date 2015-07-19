53f52e73c28936499a29495eb46c40c7a7dfddd5
Will <doggy.huang@gmail.com>
2015-07-19 09:47:06 +0800

    27 練習自訂 ActionFilter 屬性並套用在 AFController 的 Index 動作方法上 (MyFilter)

---
+++ MVC5Course/ActionFilters/MyFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.ActionFilters
+{
+    public class MyFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            File.AppendAllText(@"G:\MyFilter.log", "#1\n");
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            File.AppendAllText(@"G:\MyFilter.log", "#3\n");
+
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
---
+++ MVC5Course/Controllers/AFController.cs
@@ -0,0 +1,21 @@
+﻿using MVC5Course.ActionFilters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class AFController : Controller
+    {
+
+        [MyFilter]
+        public ActionResult Index()
+        {
+            System.IO.File.AppendAllText(@"G:\MyFilter.log", "#2\n");
+
+            return Content("Done");
+        }
+    }
+}
