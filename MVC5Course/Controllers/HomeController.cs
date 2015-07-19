a174d173a1c751289a836d0946946fd39699fb39
Will <doggy.huang@gmail.com>
2015-07-19 15:02:53 +0800

    32 練習用 Bootstrap_Metro_Dashboard 版面設計新的 Layout 頁面

--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -40,5 +40,10 @@ namespace MVC5Course.Controllers
         {
             return View();
         }
+
+        public ActionResult NewLayout()
+        {
+            return View();
+        }
     }
 }
