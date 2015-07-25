4994e52127b760978338ad2ebb1481f1db7cd357
Will <doggy.huang@gmail.com>
2015-07-25 10:46:47 +0800

    37 練習使用 @Html.DropDownList 與 SelectList 輸出 db.Occupation 的下拉選單內容

--- MVC5Course/Controllers/FormController.cs
+++ MVC5Course/Controllers/FormController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
-    public class FormController : Controller
+    public class FormController : BaseController
     {
         public ActionResult Index(SampleVM data)
         {
@@ -16,6 +16,19 @@ namespace MVC5Course.Controllers
             var dt = new SampleVM() { Name = "Will", Title = "CTO" };
 
             ViewData["Name"] = "John";
+
+            //var list = new List<SelectListItem>();
+
+            //list.Add(new SelectListItem() { Text = "進階班", Value = "3" });
+            //list.Add(new SelectListItem() { Text = "中級班", Value = "2" });
+            //list.Add(new SelectListItem() { Text = "初階班", Value = "1" });
+
+            //ViewData["LevelOptions"] = new SelectList(list, "Value", "Text", 1);
+
+
+            ViewData["LevelOptions"] = new SelectList(db.Occupation.ToList(),
+                "OccupationId", "OccupationName");
+
 
             return View(dt);
         }
