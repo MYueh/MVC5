a40c6b9223750b25bf0a4e1c979d7447a457678e
Will <doggy.huang@gmail.com>
2015-07-25 14:37:33 +0800

    41 練習使用 @Ajax.ActionLink 載入【目前時間】的資料到目前頁面中

---
+++ MVC5Course/Controllers/AjaxController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class AjaxController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetTime()
+        {
+            return Content(DateTime.Now.ToString());
+        }
+    }
+}
