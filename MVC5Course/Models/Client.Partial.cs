7bb2d2c7e21e2f1c80051c46537335ad939b165e
Will <doggy.huang@gmail.com>
2015-07-25 13:25:40 +0800

    39 練習將 Client 的 Gender 欄位修改其輸出的樣式 (顯示範本+編輯範本)

--- MVC5Course/Models/Client.Partial.cs
+++ MVC5Course/Models/Client.Partial.cs
@@ -22,7 +22,8 @@ namespace MVC5Course.Models
         
         [StringLength(40, ErrorMessage="欄位長度不得大於 40 個字元")]
         public string LastName { get; set; }
-        
+
+        [UIHint("Gender")]
         [StringLength(1, ErrorMessage="欄位長度不得大於 1 個字元")]
         public string Gender { get; set; }
         public Nullable<System.DateTime> DateOfBirth { get; set; }
