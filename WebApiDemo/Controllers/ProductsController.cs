09549a39bf76b52e8a99735badc53b519dbc2675
Will <doggy.huang@gmail.com>
2015-07-26 14:10:23 +0800

    加入 db.Configuration.LazyLoadingEnabled = false; 解決循環參考的問題

--- WebApiDemo/Controllers/ProductsController.cs
+++ WebApiDemo/Controllers/ProductsController.cs
@@ -16,6 +16,11 @@ namespace WebApiDemo.Controllers
     {
         private FabricsEntities db = new FabricsEntities();
 
+        public ProductsController()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+        }
+
         // GET: api/Products
         public IQueryable<Product> GetProduct()
         {
