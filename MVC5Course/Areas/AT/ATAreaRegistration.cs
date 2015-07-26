528ba24b38af44ce6d536b54c7fb107d81dd8825
Will <doggy.huang@gmail.com>
2015-07-26 14:33:48 +0800

    加入 ASP.NET MVC 的 Area 範例

---
+++ MVC5Course/Areas/AT/ATAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MVC5Course.Areas.AT
+{
+    public class ATAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "AT";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "AT_default",
+                "AT/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
